40c4efc6d802dfe68bcd8969cacc50285125df1e
Hanaalnuzhah <Hanaalnuzha2@gmail.com>
2022-06-05 06:06:34 -0400

    add

---
+++ src/main/java/com/example/Reservation/data/LoginRequest.java
@@ -0,0 +1,34 @@
+package com.example.Reservation.data;
+
+public class LoginRequest {
+
+    private String phoneNumber;
+    private String password;
+    private String token;
+
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+}
